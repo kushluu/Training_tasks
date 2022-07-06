146109046e5ff17b909e9c3be136c29bcef84aa7
kushluu <107112095+kushluu@users.noreply.github.com>
2022-07-06 12:55:29 +0530

    Update srikar_pwd_gen.py

--- srikar_pwd_gen.py
+++ srikar_pwd_gen.py
@@ -1,10 +1,12 @@
 import string
 import random
+from IPython.display import clear_output
 #import getpass
 lis = list(string.ascii_letters +"!@#$%^&*()" + string.digits)
 random.shuffle(lis)
 def pwd_gen():
     length=int(input())# getpass.getpass()
+    clear_output()
     password = ""
     for i in range(int(length)):
         password += random.choice(lis)
